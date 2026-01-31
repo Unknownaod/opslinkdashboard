97ceea62cef665e2600d38183d12a261ef303d40
Unknownaod <unknown@dona.pw>
2026-01-31 05:26:02 -0500

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.CLIENT_ID,
+    redirect_uri: process.env.REDIRECT_URI,
+    response_type: "code",
+    scope: "identify guilds"
+  });
+
+  res.redirect(`https://discord.com/api/oauth2/authorize?${params.toString()}`);
+}
